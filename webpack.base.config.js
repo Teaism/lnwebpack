e54023fb6ced791a49d44f6b76c3becb0f2ffa2b
陈欢 <jingfeng_quietwind@163.com>
2018-03-16 11:25:20 +0800

    fix cannot run dev

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -7,37 +7,34 @@
 
 const path = require("path");
 const webpack = require("webpack");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const extractSass = new ExtractTextPlugin({
-    filename: '[name].[contenthash].css',
-    disable: process.env.NODE_ENV === 'development'
-})
+  filename: "[name].[contenthash].css",
+  disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
-    mode: process.env.NODE_ENV === "production" ? "production" : "development",
-        entry: {
-        index: "./src/main"
-        // ,vendors: './src/vendors'
-    },
-    module: {
-        rules: [
-           /* {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader']
-                })
-            },*/
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            }
-            /*{
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  entry: {
+    index: "./src/main"
+    // ,vendors: './src/vendors'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "sass-loader"]
+        })
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+      /*{
               test: /\.scss$/,
               use: [
                   {test: /html-webpack-plugin/, use: "null-loader"},
@@ -49,31 +46,32 @@ module.exports = {
                   }
               ]
           },*/
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "src/index.tmpl.html"),
-            filename: "index.html",
-            hash: true,
-            inject: "#app"
-    })/*,
-    new ExtractTextPlugin({
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src/index.tmpl.html"),
+      filename: "index.html",
+      hash: true,
+      inject: "#app"
+    }),
+    /* new ExtractTextPlugin({
         filename: './dist/style-[hash].css'
         ,allChunks: true
     })*/
-    
+    extractSass,
     /*,
+    
     new webpack.DefinePlugin({
       PRODUCTION: JSON.stringify(true),
       DEVELOPMENT: JSON.stringify(true),
       VERSION: JSON.stringify('5fa3b9'),
       BROWSER_SUPPORTS_HTML5: true,
     })*/
-    ],
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name][hash].bundle.js",
-        chunkFilename: "[name].bundle.js"
-    }
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][hash].bundle.js",
+    chunkFilename: "[name].bundle.js"
+  }
 };
